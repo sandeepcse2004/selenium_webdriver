7fcc4c3be385e40de02258cbd7b8f3be3e778fc6
sandeepcse2004 <58394871+sandeepcse2004@users.noreply.github.com>
2020-04-18 20:52:54 +0530

    replaced find_elelemnt_by_id with By.ID to show the alternate way also to get the elements

--- textbox.py
+++ textbox.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.by import By
 
 def textbox():
     driver = webdriver.Chrome(executable_path='/Users/sandeepkumar/Documents/Selenium_Web_Automation/selenium_webdriver/chromedriver')
     driver.get("https:/www.amazon.in")
     driver.maximize_window()
-    driver.find_element_by_id('twotabsearchtextbox').send_keys('Redmi')
+    driver.find_element(By.ID, 'twotabsearchtextbox').send_keys('iphone')
     driver.find_element_by_id('twotabsearchtextbox').send_keys(Keys.RETURN)
-    time.sleep(5)
     driver.close()
 
 if __name__== '__main__':
