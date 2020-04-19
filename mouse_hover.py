7efb5fd39cb735d2e00183c6a8ac71c85c98d14a
sandeepcse2004 <58394871+sandeepcse2004@users.noreply.github.com>
2020-04-19 13:03:35 +0530

    Mouse Hover feature implementation

---
+++ mouse_hover.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time
+
+def alert():
+    driver = webdriver.Chrome(executable_path='/Users/sandeepkumar/Documents/Selenium_Web_Automation/selenium_webdriver/chromedriver')
+    driver.get("https://www.amazon.in/")
+    driver.maximize_window()
+    element = driver.find_element(By.XPATH, "//*[@id='nav-logo']/a[2]")
+    act = ActionChains(driver)
+    act.move_to_element(element).perform()
+    time.sleep(2)
+    driver.close()
+    
+
+if __name__== '__main__':
+    alert()
